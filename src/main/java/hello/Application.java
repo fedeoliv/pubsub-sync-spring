f7fbce97052da3a6701f6e92438e5c68a18b62ec
Fernando de Oliveira <fedeoliv@microsoft.com>
2019-03-06 00:34:21 +0000

    Swagger doc added

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,11 +1,14 @@
 package hello;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import hello.models.providers.Provider;
 import hello.models.providers.RedisProvider;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -5,6 +5,13 @@ import hello.models.Result;
 import hello.models.Transaction;
 import hello.models.providers.Provider;
 import hello.utils.StringUtils;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.Example;
+import io.swagger.annotations.ExampleProperty;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,9 +20,12 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.Optional;
+import java.util.Locale.Category;
+
 import static com.ea.async.Async.await;
 
 @RestController
+@Api(value="/api/transaction", description="Customer Profile", produces ="application/json")
 public class HelloController {
     @Autowired
     Provider provider;
@@ -25,6 +35,12 @@ public class HelloController {
         return "Spring API with Observer pattern";
     }
 
+    @ApiOperation(value="Start a new transaction", response = String.class)
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Success"),
+        @ApiResponse(code = 404, message = "Not Found"),
+        @ApiResponse(code = 500, message = "Timeout reached")
+    })
     @PostMapping("/api/transaction")
     public ResponseEntity<String> start(@RequestBody Transaction transaction) {
         Result result = isValidTransaction(transaction);
@@ -42,6 +58,14 @@ public class HelloController {
             : new ResponseEntity<String>("Invalid status", HttpStatus.NOT_FOUND);
     }
 
+    @ApiOperation(value="Update a transaction status", response=Transaction.class)
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Success", 
+            examples = @Example(value = { 
+                @ExampleProperty(value = "Finished", mediaType = "application/json") 
+            })),
+            
+    })
     @PutMapping("/api/transaction")
     public ResponseEntity<String> set(@RequestBody Transaction transaction) {
         Result result = isValidTransaction(transaction);
